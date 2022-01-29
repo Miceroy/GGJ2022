e227f480253ff9c0949783c28b516768b6901f9f
Mikko Romppainen <mikko.romppainen@kamk.fi>
2022-01-29 09:06:12 +0200

    Shows game results.

---
+++ Assets/ShowResultsText.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowResultsText : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TMPro.TextMeshProUGUI>().text = "TODO: Get text grom game results";
+        //GetComponent<TextMesh>().text = "TODO: Get text grom game results";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
