05da82ce5e39ff49b023ca0c579ff29451638e9d
Mikko Romppainen <mikko.romppainen@kamk.fi>
2022-01-29 15:40:46 +0200

    Level x done!

--- Assets/BridgeController.cs
+++ Assets/BridgeController.cs
@@ -4,56 +4,12 @@ using UnityEngine;
 
 public class BridgeController : MonoBehaviour, IEffectHandler
 {
-    public float rotateSpeed = 45;
-    public Transform end1;
-    public Transform end2;
-    enum State
-    {
-        OPENING,
-        OPENED,
-        CLOSING,
-        CLOSED
-    }
-    State state;
-
     float cooldown;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        end1.localEulerAngles = new Vector3(270, 0,0);
-        end2.localEulerAngles = new Vector3(-270, 0,0);
-        state = State.CLOSING;
-    }
-
-    void rotateBridge(Transform obj, float angle)
-    {
-        obj.Rotate(angle, 0, 0);
-    }
 
     // Update is called once per frame
     void Update()
     {
         cooldown -= Time.deltaTime;
-        int stopCount = 0;
-        switch (state)
-        {
-            case State.OPENING:
-                end1.localEulerAngles = new Vector3(270, 0, 0);
-                end2.localEulerAngles = new Vector3(-270, 0, 0);
-                state = State.OPENED;
-                break;
-            case State.OPENED:
-                break;
-            case State.CLOSING:
-                end1.localEulerAngles = new Vector3(0, 0, 0);
-                end2.localEulerAngles = new Vector3(0, 0, 0);
-                state = State.CLOSED;
-                break;
-            case State.CLOSED:
-                break;
-        }
-        //Debug.Log("Bridge State is now: " + state.ToString());
     }
 
 
@@ -61,25 +17,10 @@ public class BridgeController : MonoBehaviour, IEffectHandler
     {
         if (cooldown < 0)
         {
-            switch (state)
-            {
-                case State.OPENING:
-                    state = State.CLOSING;
-                    break;
-                case State.OPENED:
-                    state = State.CLOSING;
-                    break;
-                case State.CLOSING:
-                    state = State.OPENING;
-                    break;
-                case State.CLOSED:
-                    state = State.OPENING;
-                    break;
-            }
-
-            cooldown = 1;
-            //isClosing = true; !isClosed;
-            Debug.Log("Bridge State is now: " + state.ToString() );
+            Animator animator = GetComponent<Animator>();
+            animator.SetBool("OpenBridge", !animator.GetBool("OpenBridge"));
+            cooldown = 0.5f;
+            Debug.Log("Bridge State is now: " + animator.GetBool("OpenBridge").ToString() );
         }
     }
 }
