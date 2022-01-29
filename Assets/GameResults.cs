889b255de1f88abd091ee0fe88d2f356d7730e19
Mikko Romppainen <mikko.romppainen@kamk.fi>
2022-01-29 09:12:54 +0200

    GameResults added.

---
+++ Assets/GameResults.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResults : MonoBehaviour
+{
+    private static GameResults _instance;
+
+    public static GameResults Instance { get { return _instance; } }
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public bool didWin;
+}
--- Assets/ShowResultsText.cs
+++ Assets/ShowResultsText.cs
@@ -7,9 +7,8 @@ public class ShowResultsText : MonoBehaviour
     
     // Start is called before the first frame update
     void Start()
-    {
-        GetComponent<TMPro.TextMeshProUGUI>().text = "TODO: Get text grom game results";
-        //GetComponent<TextMesh>().text = "TODO: Get text grom game results";
+    { 
+        GetComponent<TMPro.TextMeshProUGUI>().text = GameResults.Instance.didWin ? "ShowResultsText.cs: Win!" : "ShowResultsText.cs: Lose!";
     }
 
     // Update is called once per frame
